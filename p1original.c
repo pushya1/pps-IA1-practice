12ac9c4ff61472b1d644c5fe7f335027a4902e90
pushya1 <86916163+pushya1@users.noreply.github.com>
2022-02-01 08:27:10 +0000

    1. Add two numbers using 4 functions and Pass by reference

--- p1original.c
+++ p1original.c
@@ -1,1 +1,33 @@
-printf("hello world");
+#include <stdio.h>
+int input()
+{
+    int a;
+    printf("Enter a number: \n");
+    scanf("%d",&a);
+    return a;
+    
+}
+
+int add(int *x, int *y) {
+   int sum;
+   
+   sum = *x + *y;
+   
+   return sum;
+}
+
+void output(int sum)
+{
+    printf("sum is %d \n",sum);
+    
+}
+ 
+int main()
+{
+    int a,b,sum;
+    a =input();
+    b =input();
+    sum = add(&a,&b);
+    output(sum);
+    return 0;
+}
